5223abc81beb750fd5980758f3b345e2b8f67183
Patrick A <akolo@hotmail.com>
2020-01-24 18:07:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,14 +26,16 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  my_hash_creator = Hash.new
-  @key = key[:key]
   
+  my_hash_creator = Hash.new
+  @key = my_hash_creator[:key]
+  @value = my_hash_creator[:value]
   
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)
