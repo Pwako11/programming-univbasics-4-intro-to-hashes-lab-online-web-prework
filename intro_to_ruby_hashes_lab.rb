3f6e43eab1c810a8624244eeee752089ec92366c
Patrick A <akolo@hotmail.com>
2020-01-24 19:08:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,13 +19,13 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
   my_hash_creator = Hash.new
   my_hash_creator = { key => value }
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  hash[key]
   
 end
 
